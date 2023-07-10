29b1faef4a1720b9392023e57a927944abd6e635
Allan Martins Lopes <133380360+Allanmlopes@users.noreply.github.com>
2023-07-10 17:15:06 -0300

    Create Buscar_url.py
    Neste Script, eu realizo o comando GetHostbyname para buscar o IP de uma URL

---
+++ Buscar_url.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import socket
+
+resp="S"
+while(resp=="S"):
+    url=input("Digite uma url: ")
+    ip=socket.gethostbyname(url)
+    print("O IP referente a url informada é: ", ip)
+    resp=input("Digite <s> para continuar :").upper()
